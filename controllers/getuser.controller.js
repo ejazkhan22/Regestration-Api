c14bf982117386f20f15dec1ede61da1fe4eefc7
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2025-05-11 20:36:28 +0500

    Update getuser.controller.js

--- controllers/getuser.controller.js
+++ controllers/getuser.controller.js
@@ -1,13 +1,1 @@
-const usermodel = require("../models/user.model")
-module.exports. getallusers = async (req, res) => {
-  try {
-    const data = await usermodel.find({});
 
-    res.status(200).json({
-      message: "get all users successful",
-      data,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
