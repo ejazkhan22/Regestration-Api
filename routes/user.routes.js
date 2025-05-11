adafd28404c39315484531c8cab3418bd50c8351
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2025-05-11 16:54:05 +0500

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -29,6 +29,6 @@ userController.loginUser
 ,
 )
 router.get("/api/profile",authMiddleware.authUser,userController.getUserProfile)
-router.get("/",getallusers)
+router.get("/",getallusers),authMiddleware.authUser,getallusers)
 
 module.exports = router;
