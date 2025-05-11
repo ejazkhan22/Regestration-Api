eed75bfc0e83d22bc6568d98d43e130e63a37393
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2025-05-11 23:35:49 +0500

    Add files via upload

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,24 +1,28 @@
-const usermodel = require("../models/user.model")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
-
+const usermodel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 const authUser = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization.split(" ")[1];
-    if (!token) {
-        return res.status(401).json({ msg: "You are not authorized" });
+  const authHeader = req.headers.authorization;
+  const token = req.cookies?.token || (authHeader && authHeader.startsWith("Bearer ") && authHeader.split(" ")[1]);
+
+  if (!token) {
+    return res.status(401).json({ msg: "You are not authorized, token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await usermodel.findById(decoded._id);
+
+    if (!user) {
+      return res.status(401).json({ msg: "User not found" });
     }
-    try {
-        const decoded = jwt.verify(token, "secretKey");
-        const user = await usermodel.findById(decoded._id);
-        if (!user) {
-            return res.status(401).json({ msg: "User not found" });
-        }
-        req.user = user;
-        next();
-    } catch (err) {
-        res.status(401).json({ msg: "You are not authorized" });
-    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error("JWT verify error:", err);
+    res.status(401).json({ msg: "You are not authorized" });
+  }
 };
 
 module.exports = { authUser };
