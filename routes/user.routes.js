7214ec5d1ec164f566bd021ef1b6bc4aed7384b4
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2025-05-11 16:37:34 +0500

    Add files via upload

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,7 +3,14 @@ const router = express.Router();
 const {body}= require("express-validator")
 const userController = require("../controllers/user.controller");
 const authMiddleware = require("../middleware/auth.middleware.js")
-router.post("/register",[
+const {authUser}= require("../middleware/auth.middleware.js")
+const {getallusers} = require('../controllers/getuser.controller')
+
+
+
+
+
+router.post("/api/register",[
     body("email").isEmail().withMessage("Email is not valid"),
     body("fullname.firstname").isLength({min:3}).withMessage("Firstname must be at least 3 characters long   "  ),
     body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")
@@ -11,7 +18,7 @@ router.post("/register",[
 userController.registerUser
 )
 
-router.post("/login",[
+router.post("/api/login",[
     body("email").isEmail().withMessage("Email is not valid"),
     body("password").isLength({min:6}).withMessage("Password must be at least 6 characters long")
 ],
@@ -21,5 +28,7 @@ userController.loginUser
 
 ,
 )
-router.get("/profile",authMiddleware.authUser,userController.getUserProfile)
+router.get("/api/profile",authMiddleware.authUser,userController.getUserProfile)
+router.get("/api/getall",getallusers)
+
 module.exports = router;
