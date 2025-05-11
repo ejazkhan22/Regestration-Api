443792f399edfab03740abb4c6e36174aed2cc8c
Ejaz khan <79781448+ejazkhan22@users.noreply.github.com>
2025-05-11 20:36:54 +0500

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -47,3 +47,16 @@ module.exports.getUserProfile= async(req,res,next)=>{
     res.status(200).json(req.user);
 
 }
+module.exports. getallusers = async (req, res) => {
+  try {
+    const data = await usermodel.find({});
+
+    res.status(200).json({
+      message: "get all users successful",
+      data,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
